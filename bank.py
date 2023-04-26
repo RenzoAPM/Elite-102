f0653646768ef2c853bf4eba474bc56ef46da31f
renzo <renzo.montgomery@gmail.com>
2023-04-26 18:40:31 -0500

    Added Deposit, Withdraw, Transfer, and View Balance. Fixed some other issues.

--- bank.py
+++ bank.py
@@ -11,11 +11,6 @@ Users - ID, Name, balance, pin, DateCreated, AccountType, Status
 '''
 
 
-
-
-
-
-
 '''
     Functions and Options
 '''
@@ -25,15 +20,15 @@ def sign_in():
     print("===========BANK===========")
     accountid = input("Account ID: ")
     accountpin = input("Account pin: ")
-    database.execute(f"select status from users where accountid='{accountid}' and pin='{accountpin}'")
-    if len(database.fetchone()):
+    database.execute(f"select status from users where accountid=%s and pin=%s", (accountid, accountpin))
+    if database.fetchone():
         signed_in = True
         while signed_in:
             signed_in = action_menu(accountid)
     else:
         os.system("clear")
         input("""===========BANK===========
-** Incorect ID or pin **
+** Incorrect ID or pin **
 Press enter to go back.
 ==========================
 """)
@@ -43,29 +38,109 @@ def new_user():
     print("===========BANK===========")
     accountname = input("Your name: ")
     accountpin = input("Your pin: ")
-    database.execute(f"insert into users (name, pin) values (%s, %s)", (accountname, accountpin))
+    database.execute("insert into users (name, pin) values (%s, %s)", (accountname, accountpin))
     database.execute("select accountid from users where accountid=last_insert_id()")
-    print(f"Your account ID is: {database.fetchone()[0]}\nRemember it.")
+    accountid = database.fetchone()[0]
     connection.commit()
+    input(f"Your account ID is: {accountid}\nRemember it.\nPress enter to continue.")
 
 def deposit(account_id):
-
+    os.system("clear")
     print("===========BANK===========\nDeposit some money.")
+    needs_number = True
+    while needs_number:
+        amount = input("Amount to deposit: ")
+        # Check if it's a number with only one "." and isn't too long
+        if amount.replace(".", "", 1).isdigit():
+            amounts = amount.split(".")
+            # Check if too many numbers are after the "."
+            if len(amounts) == 2:
+                if len(amounts[1]) <= 2:
+                    if len(amounts[0]) <= 9:
+                        needs_number = False
+                    else:
+                        print("No billionaires.")
+                else:
+                    print("Only 0.01 precision.")
+            else:
+                if len(amount) <= 9:
+                    needs_number = False
+                else:
+                    print("No billionaires.")
+        else:
+            print("That's not a number.")
+    input(float(amount))
+    database.execute("update users set balance = balance + %s where accountid = %s", (float(amount), account_id))
+    connection.commit()
     return 0
 
-def withdraw(account_id):
+def withdraw(account_id, transfer = False):
+    if not transfer:
+        os.system("clear")
+        print("===========BANK===========\nWithdraw some money.")
+    # Check the balance
+    database.execute(f"select balance from users where accountid={account_id}")
+    balance = database.fetchone()[0]
+    needs_number = True
+    while needs_number:
+        if transfer:
+            amount = input("Amount to transfer: ")
+        else:
+            amount = input("Amount to withdraw: ")
+        # Check if it's a number with only one "." and isn't too long
+        if amount.replace(".", "", 1).isdigit():
+            amounts = amount.split(".")
+            # Check if too many numbers are after the "."
+            if len(amounts) == 2:
+                if len(amounts[1]) <= 2:
+                    if float(amount) <= float(balance):
+                        needs_number = False
+                    else:
+                        print("You don't have that much.")
+                else:
+                    print("Only 0.01 precision.")
+            else:
+                if float(amount) <= float(balance):
+                    needs_number = False
+                else:
+                    print("You don't have that much.")
+        else:
+            print("That's not a number.")
+    database.execute("update users set balance = balance - %s where accountid = %s", (float(amount), account_id))
+    connection.commit()
+    if transfer:
+        return float(amount)
     return 0
 
 def transfer(account_id):
+    os.system("clear")
+    print("===========BANK===========\nTransfer some money.")
+    target_id = input("Recipient account ID: ")
+    database.execute(f"select status from users where accountid={account_id}")
+    if database.fetchone():
+        amount = withdraw(account_id, True)
+        database.execute("update users set balance = balance + %s where accountid = %s", (float(amount), target_id))
+        connection.commit()
+        input("Transfer complete.\nPress enter to continue.")
+    else:
+        input("No account found.\nPress enter to continue.")
+    return 0
+
+def check_balance(account_id):
+    os.system("clear")
+    database.execute(f"select format(balance, 2) from users where accountid={account_id}")
+    balance = database.fetchone()[0]
+    input(f"""===========BANK===========
+Your balance is ${balance}
+Press enter to continue.
+==========================
+""")
     return 0
 
 def edit_details(account_id):
     return 0
 
 def admin():
-    return 0
-
-def back():
     return 0
 
 
@@ -115,7 +190,7 @@ def action_menu(account_id):
 C - Logout
 ==========================""")
 
-    answer = take_answer(['1', '2', '3', '4', 'C'])
+    answer = take_answer(['1', '2', '3', '4', '5', 'C'])
 
     if answer == '1':
         deposit(account_id)
@@ -134,36 +209,6 @@ C - Logout
         return True
     else:
         return False
-
-
-
-def modify():
-
-    global accountid
-    global serialnumber
-    global option
-    if option == 6:
-        global newname
-        global newid
-        global newcohort
-    print(f"Option: {option}")
-
-    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-    accountid = accountid.get()
-    serialnumber = serialnumber.get()
-
-    commands = [
-        f'sshpass -p {password} ssh -t {address} echo {password} | sudo -S mysql -e"insert into history (studentid, time, type, counts, serialnumber) values ({accountid}, \'{now}\', \'checkin\', 1, \'{serialnumber}\'); insert into buffer (studentid, time, type, counts, serialnumber) values ({accountid}, \'{now}\', \'checkin\', 1, \'{serialnumber}\')" beakdb',
-        f'sshpass -p {password} ssh -t {address} echo {password} | sudo -S mysql -e"insert into history (studentid, time, type, counts, serialnumber) values ({accountid}, \'{now}\', \'checkin\', 0, \'{serialnumber}\'); insert into buffer (studentid, time, type, counts, serialnumber) values ({accountid}, \'{now}\', \'checkin\', 0, \'{serialnumber}\'); update students set chromebooks = chromebooks - 1 where studentid = {accountid}" beakdb',
-        f'sshpass -p {password} ssh -t {address} echo {password} | sudo -S mysql -e"insert into history (studentid, time, type, serialnumber) values ({accountid}, \'{now}\', \'checkout\', \'{serialnumber}\'); insert into buffer (studentid, time, type, serialnumber) values ({accountid}, \'{now}\', \'checkout\', \'{serialnumber}\'); update students set chromebooks = chromebooks + 1, chromebook = \'{serialnumber}\' where studentid = {accountid}" beakdb',
-        f'sshpass -p {password} ssh -t {address} echo {password} | sudo -S mysql -e"insert into history (studentid, time, type) values ({accountid}, \'{now}\', \'charger\'); insert into buffer (studentid, time, type) values ({accountid}, \'{now}\', \'charger\'); update students set chargers = chargers + 1 where studentid = {accountid}" beakdb',
-        f'sshpass -p {password} ssh -t {address} echo {password} | sudo -S mysql -e"insert into history (studentid, time, type) values ({accountid}, \'{now}\', \'bag\'); insert into buffer (studentid, time, type) values ({accountid}, \'{now}\', \'bag\'); update students set bags = bags + 1 where studentid = {accountid}" beakdb',
-        f'sshpass -p {password} ssh -t {address} echo {password} | sudo -S mysql -e"insert into students (studentid, name, cohort, chromebook, chromebooks, chargers, bags, new) values ({int(newid.get())}, \'{newname.get()}\', {int(newcohort.get())}, \'{serialnumber}\', 1, 1, 1, 1); insert into history (studentid, time, type, serialnumber) values ({int(newid.get())}, \'{now}\', \'checkout\', \'{serialnumber}\'); insert into buffer (studentid, time, type, serialnumber) values ({int(newid.get())}, \'{now}\', \'checkout\', \'{serialnumber}\'); insert into history (studentid, time, type) values ({int(newid.get())}, \'{now}\', \'charger\'); insert into buffer (studentid, time, type) values ({int(newid.get())}, \'{now}\', \'charger\');" beakdb'
-    ]
-
-    os.system(commands[option-1])
-    back()
 
 
 def main():
